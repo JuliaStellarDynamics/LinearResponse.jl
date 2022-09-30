281535d735ee9df80a235ca900db1ba4c2c17745
Mathieu Roule <mathieu.roule@polytechnique.edu>
2022-09-30 16:51:00 +0200

    - Redundant parameters + alpha

--- src/Utils/ParameterStructure.jl
+++ src/Utils/ParameterStructure.jl
@@ -21,15 +21,6 @@ struct ResponseParameters
     lharmonic::Int64
     n1max::Int64
     nradial::Int64
-    ndim::Int64
-
-    Ω₀::Float64
-    rmin::Float64
-    rmax::Float64
-
-    basis::AstroBasis.Basis_type
-
-    FHT::FiniteHilbertTransform.FHTtype
 
     nbResVec::Int64
     tabResVec::Matrix{Int64}
@@ -39,44 +30,48 @@ struct ResponseParameters
     KuTruncation::Int64
 
     VERBOSE::Int64
+    OVERWRITE::Bool
+
+    # Orbital Elements parameters
+    Ω₀::Float64
+    rmin::Float64
+    rmax::Float64
+    αmin::Float64
+    αmax::Float64
+
     EDGE::Float64
     ELTOLECC::Float64
-    OVERWRITE::Bool
 
 end
 
 
-function ResponseParametersCreate(basis,
-                                  FHT;
-                                  Ku=200,Kv=200,Kw=200,
-                                  modelname="model",dfname="df",
-                                  wmatdir="",gfuncdir="",modedir="",
-                                  lharmonic=2,
-                                  Ω₀=1.0,rmin=1.e-5,rmax=1.e5,
-                                  n1max=10,
-                                  nradial=10,
-                                  ndim=3,
-                                  KuTrunction=10000,
-                                  VERBOSE=0,
-                                  EDGE=0.01,ELTOLECC=0.001,OVERWRITE=false)
+function ResponseParametersCreate(dψ::Function,d2ψ::Function,basis::AstroBasis.Basis_type;
+                                  Ku::Int64=200,Kv::Int64=200,Kw::Int64=200,
+                                  modelname::String="model",dfname::String="df",
+                                  wmatdir::String="",gfuncdir::String="",modedir::String="",
+                                  lharmonic::Int64=2,n1max::Int64=10,nradial::Int64=10,
+                                  KuTruncation::Int64=10000,
+                                  VERBOSE::Int64=0,OVERWRITE=false,
+                                  Ω₀::Float64=1.0,rmin::Float64=1.e-5,rmax::Float64=1.e5,
+                                  EDGE::Float64=0.01,ELTOLECC=0.001)
 
     # Resonance vectors
-    nbResVec, tabResVec = MakeTabResVec(lharmonic,n1max,ndim)
+    nbResVec, tabResVec = MakeTabResVec(lharmonic,n1max,basis.ndim)
 
     # make the (np,nq) vectors that we need to evaluate
     tabnpnq = makeTabnpnq(nradial)
 
+    # Frequency truncation parameters
+    αmin,αmax = OrbitalElements.αminmax(dψ,d2ψ,rmin,rmax,Ω₀=Ω₀)
+
     return ResponseParameters(Ku,Kv,Kw,
                               modelname,dfname,
                               wmatdir,gfuncdir,modedir,
-                              lharmonic,n1max,nradial,ndim,
-                              Ω₀,rmin,rmax,
-                              basis,
-                              FHT,
-                              nbResVec,
-                              tabResVec,
+                              lharmonic,n1max,nradial,
+                              nbResVec,tabResVec,
                               tabnpnq,
-                              KuTrunction,
-                              VERBOSE,
-                              EDGE,ELTOLECC,OVERWRITE)
+                              KuTruncation,
+                              VERBOSE,OVERWRITE,
+                              Ω₀,rmin,rmax,αmin,αmax,
+                              EDGE,ELTOLECC)
 end
