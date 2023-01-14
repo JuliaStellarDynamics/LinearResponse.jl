44fb6b0c6e0b25b6f2b66b96d02fb36fe77aedc1
Mathieu Roule <mathieu.roule@polytechnique.edu>
2023-01-13 20:03:03 -0800

    Mandatory argument: OrbitalElements parameters + remove useless tabnpnq

--- src/Utils/ParameterStructure.jl
+++ src/Utils/ParameterStructure.jl
@@ -32,8 +32,6 @@ struct ResponseParameters
     nbResVec::Int64
     tabResVec::Matrix{Int64}
 
-    tabnpnq::Matrix{Int64}
-
     KuTruncation::Int64
 
     VERBOSE::Int64
@@ -53,27 +51,23 @@ struct ResponseParameters
 end
 
 
-function ResponseParametersCreate(;Ku::Int64=200,Kv::Int64=200,Kw::Int64=200,
+function ResponseParametersCreate(OEparams::OrbitalElements.OrbitsParameters;
+                                  Ku::Int64=200,Kv::Int64=200,Kw::Int64=200,
                                   modelname::String="model",dfname::String="df",
                                   wmatdir::String="",gfuncdir::String="",modedir::String="",
                                   lharmonic::Int64=2,n1max::Int64=10,nradial::Int64=10,
                                   KuTruncation::Int64=10000,
-                                  VERBOSE::Int64=0,OVERWRITE::Bool=false,
-                                  OEparams::OrbitalElements.OrbitsParameters,ndim::Int64=3,
+                                  VERBOSE::Int64=0,OVERWRITE::Bool=false,ndim::Int64=3,
                                   nmax::Int64,rbasis::Float64,VMAPN::Int64=2,ADAPTIVEKW::Bool=false)
 
     # Resonance vectors
     nbResVec, tabResVec = MakeTabResVec(lharmonic,n1max,ndim)
-
-    # make the (np,nq) vectors that we need to evaluate
-    tabnpnq = makeTabnpnq(nradial)
 
     return ResponseParameters(Ku,Kv,Kw,
                               modelname,dfname,
                               wmatdir,gfuncdir,modedir,
                               lharmonic,n1max,nradial,
                               nbResVec,tabResVec,
-                              tabnpnq,
                               KuTruncation,
                               VERBOSE,OVERWRITE,
                               OEparams,
